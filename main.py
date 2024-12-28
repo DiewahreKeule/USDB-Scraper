2d9939c30db7ed637c0f37fd53b4785f11b8202c
DiewahreKeule <106590312+DiewahreKeule@users.noreply.github.com>
2024-12-28 18:45:39 +0100

    #1 Logging - Add Logging Routine - Code cleaning

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import threading
 import requests
 import re
 import feedparser
+import logging
 
 # Load Config-File
 usdbUtilityObj = USDBUtility()
@@ -16,16 +17,21 @@ config = usdbUtilityObj.load_config('config.json')
 
 app = Flask(__name__)
 
+# Create Flask Logger
+flask_logger = logging.getLogger('flask_logger')
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p', filename='flask_scraper.log', encoding='utf-8', level=logging.DEBUG)
+
+
 async def background_task():    
-    """
-    Eine asynchrone Funktion, die in einem Zyklus läuft und alle 60 Sekunden eine Aufgabe ausführt.
+    """    
+    Asyncrone function, that runs in a cycle and execute a task every 20 seconds.
     """
 
     usdbUtilityObj = USDBUtility()
     databaseObj = USDBScraperDB("usdb_scrapper.db")    
 
-    while True:
-        print("Hintergrundaufgabe wird ausgeführt...")
+    while True:        
+        flask_logger.debug("Background Task is running...")
 
         # Create Database Object        
         query_song_list = databaseObj.fetch_all_songs()
@@ -35,23 +41,30 @@ async def background_task():
             usdb_song_id = song.get("USDB_SONG_ID")
 
             # STATUS 0  ->  Scrapping
-            if song.get("STATUS") == 0:                
-                print("Get Song Informationen from Database")
+            if song.get("STATUS") == 0:                                
+                print("STATUS 0 -> Get Song Information from Database - USDB_SONG_ID: " + str(usdb_song_id))
+                flask_logger.debug("STATUS 0 -> Get Song Information from Database - USDB_SONG_ID: " + str(usdb_song_id))
                 
                 # Update Song Status -> 1 (Scraping)
                 databaseObj.update_song_status(usdb_song_id, 1)
 
-                # USDB Config
+                # USDB Config to Variable
                 usdb_config = config["USDBScraper"]                    
 
-                # 1. Anhand der USDB-ID die Song Informationen scrapen und in die Datenbank speichern                                                
+                flask_logger.debug("USDB Scraping start...")
+
+                # Scraping Song Information from USDB                                            
                 usdbScraperObject = USDBScraper("./chromedriver-win64/chromedriver.exe", usdb_config.get("USERNAME", ""), usdb_config.get("PASSWORD", "")) 
                 song_infos = usdbScraperObject.srape_song(usdb_song_id)  
+
+                # Update Song in Database
                 databaseObj.upate_song_by_id(usdb_song_id, song_infos.get("YOUTUBE_URL", ""), song_infos.get("SONG_TEXT", ""), 2)
+                flask_logger.debug("USDB Scraping end...")
 
             # STATUS 2  -> Scraping Completed
             if song.get("STATUS") == 2:
-                print("Download Youtube Video / Download mp3 / Create Folder")
+                print("STATUS 2 -> Download Video / mp3 / Create Folder - USDB_SONG_ID: " + str(usdb_song_id))
+                flask_logger.debug("STATUS 2 -> Download Video / mp3 / Create Folder - USDB_SONG_ID: " + str(usdb_song_id))
 
                 # Application Config
                 application_config = config["APPLICATION"] 
@@ -59,50 +72,58 @@ async def background_task():
                 # Update Song Status -> 3 (Downloading / Create Folder)
                 databaseObj.update_song_status(usdb_song_id, 3)
 
-                # 1. Ordner erstellen                
+                # 1. Create Folder
+                flask_logger.debug("Create Song Folder...")               
                 song_folder_name = song.get("SONG_INTERPRET") + "-" + song.get("SONG_TITLE")  
                 output_path  = usdbUtilityObj.create_song_folder(song_folder_name, application_config.get("OUTPUT_DIRECTORY", "output"))
                 media_file_path = output_path + "/" + song_folder_name
-
-                # 2. Lyrics in eine Textdatei speichern (Dateiname: SONG_INTERPRET - SONG_TITLE.txt)    
+                
+                # 2. Save Lyrics in a text file (Filename: SONG_INTERPRET - SONG_TITLE.txt)
+                flask_logger.debug("Create Song Lyrics File...")      
                 song_text = song.get('SONG_ULTRA_STAR_LYRICS')
-                # Datei speichern
+
+                # 3. Save File
                 output_file = media_file_path + ".txt"
                 with open(output_file, "w", encoding="utf-8") as file:
                     file.write(song_text)
-                
-                # 5. Anhand YOUTUBE_LINK das Video und die MP3 herunterladen und wie oben beschrieben benennen    
-                usdbUtilityObj.youtube_to_mp3(song.get('SONG_YT_VIDEO_LINK'), media_file_path)    
+                                
+                # 4. Download Audio and Video from Youtube
+                flask_logger.debug("Download Audio...")   
+                usdbUtilityObj.youtube_to_mp3(song.get('SONG_YT_VIDEO_LINK'), media_file_path) 
+                 
+                flask_logger.debug("Download Video...") 
                 usdbUtilityObj.youtube_to_mp4(song.get('SONG_YT_VIDEO_LINK'), media_file_path)
                 
-                # 6. COVER herunterladen und umbenennen    
+                # 5. Download Cover Image
+                flask_logger.debug("Download Cover...")   
                 image_path = output_path + "/" + song_folder_name + ".jpg"
                 usdbUtilityObj.download_image(song.get('SONG_COVER_URL'), image_path)    
 
                 # Update Song Status -> 3 (Downloading / Create Folder)
                 databaseObj.update_song_status(usdb_song_id, 4)
+                flask_logger.debug("Download Completed...")
 
                 # prepare Song for UltraStar
                 usdbUtilityObj.prepare_song_ultra_star(output_path)
 
-        
-        await asyncio.sleep(20)  # 60 Sekunden warten
+        flask_logger.debug("Background Task finished / wait 20 seconds...")
+        await asyncio.sleep(20)  # wait 20 seconds
 
 def start_background_task():
     """
-    Startet die asynchrone Hintergrundaufgabe in einem separaten Thread.
+    start the background task in a new event loop
     """
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(background_task())
 
-# Serverseitige Liste, um die Song-IDs zu speichern
+# Save the Song-IDs on the Server   
 download_list = []
 
 # PAGES
 @app.route('/', methods=['GET'])
-def page_home():
-    """Gibt die Songliste als JSON zurück."""
+def page_home():    
+    flask_logger.debug("GET: Home Page...")
 
     feed_url_top_10 = "https://usdb.animux.de/rss/rss_new_top10.php"
     feed_data = feedparser.parse(feed_url_top_10)
@@ -116,18 +137,15 @@ def page_home():
 
 @app.route('/search-song', methods=['GET'])
 def page_search_song():
-    """Gibt die Songliste als JSON zurück."""
-
-    # SONGS = usdbScraperObject.search_song("Willst du")
-
-    # return jsonify(SONGS)
-    # return render_template('songs.html', songs=SONGS)
+    flask_logger.debug("GET: Search Song Page...")
+    
     return render_template('songs.html')
 
 @app.route('/query-list', methods=['GET'])
 def page_query_list():
+    flask_logger.debug("GET: Query Page...")
 
-    # Databse Object
+    # Create Databse Object
     databaseObj = USDBScraperDB("usdb_scrapper.db")
     query_song_list = databaseObj.fetch_all_songs()  
     
@@ -135,6 +153,7 @@ def page_query_list():
 
 @app.route('/downloads', methods=['GET'])
 def page_downloads():
+    flask_logger.debug("GET: Downloads Page...")
 
     # Databse Object
     databaseObj = USDBScraperDB("usdb_scrapper.db")    
@@ -144,15 +163,18 @@ def page_downloads():
 
 @app.route('/ultrastar-tools', methods=['GET'])
 def page_ultrastar_tools():
+    flask_logger.debug("GET: UltraStar Tools Page...")
     return render_template('ultrastar_tools.html')
 
 # FUNCTIONS
 @app.route('/search', methods=['GET'])
 def search():
     """Führt die Suche durch und gibt die Ergebnisse als JSON zurück."""
+    flask_logger.debug("GET: Search Song...")
     query = request.args.get('query', '').lower()
     filter_by = request.args.get('filter', 'TITLE')        
     print(filter_by)
+    flask_logger.debug("Search Filter: " + str(filter_by))
 
     # USDB Config
     usdb_config = config["USDBScraper"]    
@@ -167,6 +189,8 @@ def function_query_list_action():
     usdb_song_id = request.json.get('USDB_SONG_ID')
     status = request.json.get('STATUS')
 
+    flask_logger.debug("Change Song Status: " + str(usdb_song_id) + " - " + str(status))
+
     databaseObj = USDBScraperDB("usdb_scrapper.db")
     databaseObj.update_song_status(usdb_song_id, status)
 
@@ -174,27 +198,30 @@ def function_query_list_action():
 
 @app.route('/add_to_download', methods=['POST'])
 def add_to_download():
-    """Fügt einen Song mit allen Details zur Download-Liste hinzu."""
+    """Fügt einen Song mit allen Details zur Download-Liste hinzu."""    
+
     song_id = request.json.get('SONG_ID')
     song_interpret = request.json.get('SONG_INTERPRET')
     song_title = request.json.get('SONG_TITEL')
 
+    flask_logger.debug("Add Song to Downloadlist: " + str(song_id) + " - " + str(song_interpret) + " - " + str(song_title))
+
     song_cover_url = "https://usdb.animux.de/data/cover/" + song_id + ".jpg"
 
-    # Song in die Datenbank speichern
+    # Save Song to Database
     databaseObj = USDBScraperDB("usdb_scrapper.db")
     databaseObj.insert_song_query(song_id, song_title, song_interpret, song_cover_url)
 
-    # Validiere die Daten
+    # Data Validation
     if not (song_id and song_interpret and song_title):
         return jsonify({"status": "error", "message": "Ungültige Song-Daten"}), 400
 
-    # Überprüfen, ob der Song bereits in der Liste ist
+    # Check if Song is already in the List
     for song in download_list:
         if song['SONG_ID'] == song_id:
             return jsonify({"status": "info", "message": f"Song mit ID {song_id} ist bereits in der Liste."})
 
-    # Song zur Liste hinzufügen
+    # Add Song to List
     download_list.append({
         "SONG_ID": song_id,
         "SONG_INTERPRET": song_interpret,
@@ -205,10 +232,9 @@ def add_to_download():
 
 @app.route('/remove_from_download', methods=['POST'])
 def remove_from_download():
-    """Entfernt einen Song aus der Download-Liste."""
     song_id = request.json.get('SONG_ID')
 
-    # Validiere die Song-ID
+    # Song ID Validation
     if not song_id:
         return jsonify({"status": "error", "message": "Keine Song-ID übermittelt"}), 400
 
@@ -226,8 +252,7 @@ def get_download_list():
     return jsonify(download_list)
 
 if __name__ == '__main__':
-
-    # HIER
+    flask_logger.debug("Start USDB-Scraper Webserver...")    
     
     # Check config
     if config:
@@ -242,5 +267,5 @@ if __name__ == '__main__':
         background_thread.start()
 
         app.run(host=ip, port=port, debug=debug)
-    else:
-        print("Can't find config.json!")
+    else:        
+        flask_logger.error("Can't find config.json!")
